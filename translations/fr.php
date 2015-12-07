09bc5bfd63ceb2f344699bf087a5ad2968d8b4e9
cam.lafit <cam.lafit@azerttyu.net>
2015-12-07 11:13:30 +0100

    French translation
    * Add a new transalation : french

---
+++ translations/fr.php
@@ -0,0 +1,35 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{imp_cookies}prestashop>imp_cookies-older_f76722211e4724e5686d1bf1bcebbbdd'] = 'J\'accepte';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies-older_7d5a27f1b53dc519eac7bb288f664892'] = 'En savoir plus';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_6f085db52ce3b7fcaa74e69191b73316'] = 'Loi sur l\'information des cookies';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_8e2922e12f108f0ad084dffca0a0dcf1'] = 'Demander à votre client d\'accepter ou non ';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_505a83f220c02df2f85c3810cd9ceb38'] = 'Réussie';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_f4f70727dc34561dfde1a3c529b6205c'] = 'Paramétrage';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_9dffbf69ffba8bc38bc4e01abf4b1675'] = 'Texte libre';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_2589e95edc88ca697a496faec730ec7e'] = 'Lien vers la page CMS';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_368d9ac76af05f714092bc808a426bfc'] = 'Couleur de fond';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_5f111ae4c490902059da2004cbc8b424'] = 'Couleur de texte';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_d23d825cf3ed4d23b61586c0e6334815'] = 'Opacité';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_f6eecde1528a48773e3b7af69db1447f'] = 'entre 0.1 et 1';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_8dbffc9b2355846a286505bfd1cba33d'] = 'Largeur ';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_8588977a4fd94cc5b96ec29c112e2e31'] = 'en \"%\" ou \"px\"';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_218d5fedf7eac0c4f6f3bc0422f2c919'] = 'Position de la barre';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_4696d19e974756d774e420f88bc868f2'] = 'en haut de page';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_e7980fddf41a86fe4f6557ecc59c3866'] = 'en pied de page';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_8a8ada7e7cc13b47c047f226d1e08926'] = 'padding';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_2f9ae8868d0b82bd20c7a247ff2d3b4c'] = 'espace dans le bloc, en \"px\"';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_52dbb8b29caba5a878d3e1511676eb7f'] = 'arronds des bordures';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_ebfaf9557d30e072e54bdf9d94f1e772'] = 'épaisseur de la bordure, en \"px\"';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_e271e62784a2c9b801535a7bcaffd460'] = 'marges';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_3c1067f4002e8d771dff4e208ff3ccbb'] = 'marges, en \"px\" : haut droite bas gauche, par exemple 20px 0 20px 0';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_c22b28c5c38d219b521e476691bd7158'] = 'Alignement du texte';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_811882fecd5c7618d7099ebbd39ea254'] = 'Gauche';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_adb115059e28d960fa8badfac5516667'] = 'Centré';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_7c4f29407893c334a6cb7a87bf045c0d'] = 'Droite';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_7a8dadb65ef048ed26e60c420b6ada15'] = 'Z-index (utilisateurs avancés)';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_c9cc8cce247e49bae79f15173ce97354'] = 'Sauvegarder';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_f76722211e4724e5686d1bf1bcebbbdd'] = 'J\'accepte';
+$_MODULE['<{imp_cookies}prestashop>imp_cookies_7d5a27f1b53dc519eac7bb288f664892'] = 'En savoir plus';
